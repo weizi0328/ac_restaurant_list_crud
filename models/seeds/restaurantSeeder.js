ad9227185fb8b1fe8420686f467c4e31fc7847ae
weizi0328 <ankewang26@gmail.com>
2022-07-06 14:26:52 +0800

    correct mongoose connection

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,18 +1,30 @@
 const mongoose = require('mongoose')
 const Restaurant = require('../restaurant')  // 載入 restaurant model
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
-const db = mongoose.connection
+// const db = mongoose.connection
 
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+// db.on('error', () => {
+//   console.log('mongodb error!')
+// })
 
-db.once('open', () => {
-  console.log('mongodb connected!')
-  for (let i = 0; i < 10; i++) {
-    Restaurant.create({ name: `name-${i}` })
-  }
-  console.log('done')
-})
+// db.once('open', () => {
+//   console.log('mongodb connected!')
+//   for (let i = 0; i < 10; i++) {
+//     Restaurant.create({ name: `name-${i}` })
+//   }
+//   console.log('done')
+// })
+
+mongoose.connect('mongodb+srv://alpha:camp@cluster0.2rlpc.mongodb.net/restaurant-list?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((mongoose) => {
+    const db = mongoose.connection
+    db.once('open', () => {
+      console.log('connected')
+      for (let i = 0; i < 10; i++) {
+        Restaurant.create({ name: `name-${i}` })
+      }
+    })
+  })
+  .catch(error => console.log(error))
